ebbb90adfa740eb4cff0c2de9f5664c1a3b7d028
webprogrammer77 <webprogrammer77@gmail.com>
2019-10-07 16:52:29 +0300

    disabled main.js

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -25,6 +25,7 @@ $(function() {
     '<div class="success_wrapper smart-basket__alert-wrapper"><div class="smart-basket__alert"><div class="smart-basket__alert-icon"><svg class="" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2"><circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"></circle><polyline class="path check" fill="none" stroke="#73AF55" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 "></polyline></svg></div><div class="smart-basket__alert-text">Спасибо за покупку! <br> Наш менеджер свяжется с вами.</div><div class="smart-basket__alert-footer" style="justify-content:center;"><button class="smart-basket__alert-button smart-basket__alert-button_close success">Продолжить покупки</button></div></div></div>';
 
   $(document).on("click", ".smart-basket__send-form", function(e) {
+		$(this).prop("disabled", true);
     $("body").css("overflow", "auto");
     setTimeout(function() {
       $(".smart-basket").toggleClass("smart-basket_active");
